44d5fbefbca82a748680a13a91c91deadebbe54e
a-bhuyan <arnob.bhuyan@gmail.com>
2017-08-09 06:28:45 -0500

    Added the rest of the files, still need to make edits to get into final working order

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,9 @@ USE burgers_db;
 
 CREATE TABLE burgers (
    id INT NOT NULL AUTO_INCREMENT,
-   burger_name VARCHAR(50) NOT NULL,
+   burger_name VARCHAR(255) NOT NULL,
    devoured BOOLEAN NOT NULL DEFAULT 0,
-   date TIMESTAMP
+   date TIMESTAMP,
+   PRIMARY KEY (id)
+
 );
